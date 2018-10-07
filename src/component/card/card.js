7bfe2bf9cccc19d9c3b62866c9752c1c84e078f4
Zoe Zhao <zoefyz12@gmail.com>
2018-10-06 22:33:19 -0700

    rename all image and able to add to card

--- src/component/card/card.js
+++ src/component/card/card.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 import './card.scss';
 
-const card = () => {
+const importAll = (r) => {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item);
+        return null;
+    });
+    return images;
+};
+
+// refer https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+const images = importAll(require.context('./../../assist/img', false, /\.(png|jpe?g|svg)$/));
+
+const card = ({cardInfo}) => {
     return (
         <div className='card-container'>
-
+            <img src={images[cardInfo.serialNum + '.svg']} alt='icon'/>
         </div>
     );
 };
--- src/component/game/game.js
+++ src/component/game/game.js
@@ -9,7 +9,15 @@ class Game extends Component {
     constructor(props) {
         super(props);
         let cardGrid = [];
+        let iconLayout = [];
         // row col
+
+        for (let i = 1; i <= Math.pow(props.gameLevel.cardRow, 2) / 2; i++) {
+            iconLayout.push(i);
+            iconLayout.push(i);
+        }
+
+        iconLayout = this.createRandomLayout(iconLayout);
 
         for (let i = 0; i < props.gameLevel.cardRow; i++) {
             let rowGrid = [];
@@ -17,7 +25,8 @@ class Game extends Component {
             for (let j = 0; j < props.gameLevel.cardRow; j++) {
                 rowGrid.push({
                     matched: false,
-                    flip: false
+                    flip: false,
+                    serialNum: iconLayout.pop()
                 });
             }
 
@@ -26,7 +35,9 @@ class Game extends Component {
 
         this.state = {
             cardGrid: cardGrid,
-            timeLimit: props.gameLevel.time
+            timeLimit: props.gameLevel.time,
+            gameOver: false,
+            win: false
         };
     }
 
@@ -38,9 +49,27 @@ class Game extends Component {
 
     };
 
-    render() {
+    // refer to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    createRandomLayout = (array) => {
+        let currentIndex = array.length, temporaryValue, randomIndex;
 
-        console.log(this.state.cardGrid)
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    };
+
+    render() {
         return (
             <div className='game-container'>
 
@@ -52,19 +81,20 @@ class Game extends Component {
                                              showMilliseconds={true}
                                              size={100}
                                              alpha={0.6}
-                                             onComplete={this.timeFinished} />
+                                             onComplete={this.timeFinished}/>
                     </div>
 
                     <div className='game-section'>
 
                         {
-                            this.state.cardGrid.map((val, index) => {
+                            this.state.cardGrid.map((val, rowIndex) => {
                                 return (
-                                    <div className='row mb-3'>
-                                        {val.map((val, index) => {
+                                    <div className='row mb-3' key={'row' + rowIndex}>
+                                        {val.map((val, colIndex) => {
                                             return (
-                                                <div className={'col-' + (12 / this.state.cardGrid.length).toString()}>
-                                                    <Card/>
+                                                <div key={rowIndex + colIndex}
+                                                     className={'col-' + (12 / this.state.cardGrid.length).toString()}>
+                                                    <Card cardInfo={val}/>
                                                 </div>
                                             )
                                         })}
